004799cea0a96b4c5374c42573dbf90750c53632
Bobby Yost <bobby.9202@gmail.com>
2020-01-16 16:45:47 -0500

    fix linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { JwtInterceptor } from "src/app/helpers/jwt.interceptor";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ErrorInterceptor } from "./helpers/error.interceptor";
-import { fakeBackendProvider } from "./helpers/fake-backend";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtInterceptor } from 'src/app/helpers/jwt.interceptor';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ErrorInterceptor } from './helpers/error.interceptor';
+import { fakeBackendProvider } from './helpers/fake-backend';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,4 +20,4 @@ import { fakeBackendProvider } from "./helpers/fake-backend";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
