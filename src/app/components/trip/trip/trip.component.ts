f0723dc54f27836134b327264c1c3206cd509662
Ryan <ryanthomas1998@gmail.com>
2020-01-21 16:42:09 -0500

    fixed merge conflicts

--- src/app/components/trip/trip/trip.component.ts
+++ src/app/components/trip/trip/trip.component.ts
@@ -12,6 +12,42 @@ export class TripComponent implements OnInit {
 
   showTrips = false;
   isLoading = false;
+
+  trips = [{
+    region: 'French Polynesia',
+    img: 'assets/frenchpoly.jpg',
+    description: 'The last few years have been challenging for you, but also empowering — and you’re ready to make some big changes in 2020. You’ve overcome a lot of obstacles and have a clear vision of what you want. Kicking off the year with a wellness retreat will be just what the doctor ordered.',
+    flights: [],
+    stops: [
+      {
+        location: 'Bora Bora'
+      },
+      {
+        locaiton: 'Tahiti'
+      },
+      {
+        location: 'Ta Ha\'a'
+      }
+    ]
+  },
+  {
+    region: 'Taiwan',
+    img: 'assets/taiwain.jpg',
+    description: 'You’ve been very independent from a young age, and get a thrill out of making decisions and plans for yourself. When you travel with a close group of friends, they know they can count on you for handling the itinerary and all the reservations. When push comes to shove, though, you’d rather just do a solo trip. We think it’s about time you head to Taiwan.',
+    flights: [],
+    stops: [
+      {
+        location: 'Taipei'
+      },
+      {
+        locaiton: 'Sun Moon Lake'
+      },
+      {
+        location: 'Taichung City'
+      }
+    ]
+  }
+  ];
   constructor(private formBuilder: FormBuilder) { }
 
   validate() {
