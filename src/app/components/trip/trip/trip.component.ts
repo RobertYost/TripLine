c207869aa42ee68eb0e3cae9eaf88f651d9bbad9
Ryan <ryanthomas1998@gmail.com>
2020-01-21 17:11:15 -0500

    added a bunch to the trip layout and made the cards expand

--- src/app/components/trip/trip/trip.component.ts
+++ src/app/components/trip/trip/trip.component.ts
@@ -10,40 +10,87 @@ export class TripComponent implements OnInit {
 
   form: FormGroup;
 
-  showTrips = false;
+  showTrips = true;
   isLoading = false;
 
   trips = [{
     region: 'French Polynesia',
+    expanded: true,
+    total_price: '$8,350',
     img: 'assets/frenchpoly.jpg',
     description: 'The last few years have been challenging for you, but also empowering — and you’re ready to make some big changes in 2020. You’ve overcome a lot of obstacles and have a clear vision of what you want. Kicking off the year with a wellness retreat will be just what the doctor ordered.',
-    flights: [],
+    flights: [
+      {
+        route: 'Columbus - Bora Bora',
+        takeoff_time: '5:43pm',
+        landing_time: '12:30pm',
+        price: '$743.75'
+      },
+      {
+        route: 'Bora Bora - Tahiti',
+        takeoff_time: '5:43pm',
+        landing_time: '6:15pm',
+        price: '$243.00'
+      },
+      {
+        route: 'Tahiti - Ta Ha\'a',
+        takeoff_time: '5:43pm',
+        landing_time: '12:30pm',
+        price: '$137.75'
+      },
+      {
+        route: 'Ta Ha\'a - Columbus',
+        takeoff_time: '5:43pm',
+        landing_time: '12:30pm',
+        price: '$643.75'
+      }
+    ],
     stops: [
       {
-        location: 'Bora Bora'
+        location: 'Bora Bora',
+        arrival_date: '02/04/2020',
+        hotel_cost: '$499.90 per night',
+        food_cost: '$185.00 per day '
       },
       {
-        locaiton: 'Tahiti'
+        location: 'Tahiti',
+        arrival_date: '02/9/2020',
+        hotel_cost: '$799.0 per night',
+        food_cost: '$199.00 per day'
       },
       {
-        location: 'Ta Ha\'a'
+        location: 'Ta Ha\'a',
+        arrival_date: '02/11/2020',
+        hotel_cost: '$899.00 per night',
+        food_cost: '$265.00 per day'
       }
     ]
   },
   {
     region: 'Taiwan',
+    expanded: false,
+    total_price: '$2,025',
     img: 'assets/taiwain.jpg',
     description: 'You’ve been very independent from a young age, and get a thrill out of making decisions and plans for yourself. When you travel with a close group of friends, they know they can count on you for handling the itinerary and all the reservations. When push comes to shove, though, you’d rather just do a solo trip. We think it’s about time you head to Taiwan.',
     flights: [],
     stops: [
       {
-        location: 'Taipei'
+        location: 'Taipei',
+        arrival_date: '02/04/2020',
+        hotel_cost: '$49.00 per night',
+        food_cost: '$7.00 per day'
       },
       {
-        locaiton: 'Sun Moon Lake'
+        location: 'Sun Moon Lake',
+        arrival_date: '02/09/2020',
+        hotel_cost: '$79.00 per night',
+        food_cost: '$9.00 per day'
       },
       {
-        location: 'Taichung City'
+        location: 'Taichung City',
+        arrival_date: '02/11/2020',
+        hotel_cost: '$65.00 per night',
+        food_cost: '$6.50 per day'
       }
     ]
   }
